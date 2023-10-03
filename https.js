8cede421c7dca45eb987be2d9b9193ef8f2e70c3
Jyotiswaroop Srivastav <srivastavjyotiswaroop@gmail.com>
2023-10-03 13:48:12 +0530

    Finished with http

--- https.js
+++ https.js
@@ -1,15 +1,23 @@
 const http = require("http");
+const { readFileSync } = require("fs");
+
+const Homepage = readFileSync("./index.html");
 
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
-    res.end("Homepage");
-  }
-  if (req.url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(Homepage);
+  } else if (req.url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/html" });
     res.end("About");
-  }
-  res.end(`
+  } else {
+    res.writeHead(400, { "Content-Type": "text/html" });
+    res.end(`
     404 Not found!
     <a href="/">Homepage</a>`);
+  }
 });
 
-server.listen(3000);
+server.listen(3000, () => {
+  console.log("Server running at http://localhost:3000/");
+});
